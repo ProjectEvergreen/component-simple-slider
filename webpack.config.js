6ba1e4208f962159c955a197470840c0d9953997
Owen Buckley <owen@thegreenhouse.io>
2018-06-10 13:44:56 -0400

    clean up config

--- webpack.config.js
+++ webpack.config.js
@@ -34,22 +34,3 @@ module.exports = {
   ]
 
 };
-
-// const commonConfig = require('./webpack.config.common');
-// const webpackMerge = require('webpack-merge');
-
-// module.exports = webpackMerge(commonConfig, {
-
-//   mode: 'development',
-  
-//   devServer: {
-//     port: 1981,
-//     host: 'localhost',
-//     historyApiFallback: true,
-//     watchOptions: {
-//       aggregateTimeout: 300,
-//       poll: 1000
-//     }
-//   }
-
-// });

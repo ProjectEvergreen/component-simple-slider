f18bbab9e07f4859545e2848f25a1060bdb98af9
Owen Buckley <owenbuckley13@gmail.com>
2018-07-29 11:57:11 -0400

    move tests into JS files

---
+++ test/hello-world.spec.js
@@ -0,0 +1,24 @@
+describe('HelloComponent', function() {
+  let componentRoot;
+
+  beforeEach(() => {
+    componentRoot = fixture('hello').root;
+  });
+
+  describe('heading', () => {
+    let heading;
+
+    beforeEach(() => {
+      heading = componentRoot.querySelectorAll('h3.heading');
+    });
+
+    it('should have one heading element', () => {
+      expect(heading.length).to.equal(1);
+    });
+
+    it('should say Hello World!', () => {
+      expect(heading[0].innerHTML).to.equal('Hello World!');
+    });
+  });
+
+});
